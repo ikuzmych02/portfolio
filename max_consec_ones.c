ce22fa5b63ca2dfd415b8acae76d19236694d01e
iluzmych <iluzmych@uw.edu>
2022-11-25 19:02:31 -0800

    fixed formatting issues

--- max_consec_ones.c
+++ max_consec_ones.c
@@ -1,6 +1,27 @@
 #include "Helpers.h"
 
+int countConsecutiveOnes(int input);
 
+int main() {
+    int MK_VAR(0), MK_VAR(1);
+
+    result0 = countConsecutiveOnes(0xF0000001);
+    result1 = countConsecutiveOnes(0xED00058A);
+
+    printf("Count of consecutive ones in result0 is %d\n", result0); // should output 4
+    printf("Count of consecutive ones in result1 is %d\n", result1); // should output 3. Expands to: 11101101000000000000010110001010
+
+    return 0;
+}
+
+/**
+ * This function takes an integer input and counts maximum amount of consecutive
+ * bits in the binary representation of the number
+ * 
+ * @param input   Integer input
+ * 
+ * @return        Count of max consecutive ones
+*/
 int countConsecutiveOnes(int input) {
     int count = 0;
     int max = 0;
@@ -22,17 +43,4 @@ int countConsecutiveOnes(int input) {
     }
     
     return max;
-}
-
-
-int main() {
-    int MK_VAR(0), MK_VAR(1);
-
-    result0 = countConsecutiveOnes(0xF0000001);
-    result1 = countConsecutiveOnes(0xED00058A);
-
-    printf("Count of consecutive ones in result0 is %d\n", result0); // should output 4
-    printf("Count of consecutive ones in result1 is %d\n", result1); // should output 3. Expands to: 11101101000000000000010110001010
-
-    return 0;
 }
