2ee7884aeacb64022e0c6299a280d29feb5c39cf
iluzmych <iluzmych@uw.edu>
2022-11-25 18:20:42 -0800

    optimized reverse_string to remove extra for loop. now done in place

--- reverse_string.c
+++ reverse_string.c
@@ -1,32 +1,11 @@
 #include "Helpers.h"
 
-/**
- * Function that will reverse a string in-place
- * 
- * @param str String of any size
-*/
-void reverseString(char* str) {
-    char *temp = (char*) malloc(sizeof(char) * strlen(str));
-    int j = strlen(str) - 1;
-    for (int i = 0; i < strlen(str); i++) {
-        temp[i] = str[j];
-        j--;
-    }
-
-    for (int i = 0; i < strlen(str); i++) {
-        str[i] = temp[i];
-    }
-
-    free(temp);
-    return;
-}
-
+void reverseString(char* str);
 
 int main() {
     char* result = (char*)(malloc(10));
     
     memcpy(result, "Forty-two", sizeof(char)*9 + 1);
-    printf("%d\n", strlen(result));
     printf("%s\n", result);
 
     reverseString(result);
@@ -37,4 +16,21 @@ int main() {
     result = NULL;
 
     return 0;
+}
+
+/**
+ * Function that will reverse a string in-place
+ * 
+ * @param str String of any size
+*/
+void reverseString(char* str) {
+    
+    char temp;
+    for (int i = 0; i < strlen(str) / 2; i++) {
+        temp = str[i];
+        str[i] = str[strlen(str) - 1 - i];
+        str[strlen(str) - 1 - i] = temp;
+    }
+
+    return;
 }
