938cc44783f65ffa8893dd60a72fc2f8e494f600
iluzmych <iluzmych@uw.edu>
2022-12-04 20:49:38 -0800

    update xor_without_operator
    optimized the code

--- xor_without_operator.c
+++ xor_without_operator.c
@@ -3,14 +3,16 @@
 int xor(int a, int b);
 
 int main() {
-    int res1, res2, res3;
+    int res1, res2, res3, res4;
 
     res1 = xor(9, 9);
     res2 = xor(12, 0);
     res3 = xor(17, 3);
-    printf("9 xor 9 is : %d\n", res1);
-    printf("12 xor 0 is: %d\n", res2);
-    printf("17 xor 3 is: %d\n", res3);
+    res4 = xor(29, 17);
+    printf("9 xor 9 is : %d\n", res1); // 0
+    printf("12 xor 0 is: %d\n", res2); // 12
+    printf("17 xor 3 is: %d\n", res3); // 18
+    printf("29 xor 17 is: %d\n", res4); // 12
 
     return 0;
 }
@@ -25,11 +27,9 @@ int main() {
  * @return res    Output after doing the XOR of input 1 and input 2
 */
 int xor(int a, int b) {
-    int res = 0;
-
-    for (int i = 0; i < sizeof(int) * 8; i++) {
-        if (((a >> i) & 1) != ((b >> i) & 1))
-            res |= 1 << i;
-    }
-    return res;
+    // Result itself is fairly simple.
+    // In an XOR, we can only have one of the numbers be true at any bit position
+    // This checks if a & NOT b is true, or if NOT a and b is true. The or
+    // gate at the end gets the final result.
+    return (a & ~b) | (~a & b);
 }
